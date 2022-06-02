bf481f4493890c05fa1a2249795d021c1cd89b86
Nuno Fernandess <me@nfriacowboy.com>
2022-06-02 20:45:59 +0000

    handling request services

--- src/main/kotlin/pt/nfriacowboy/presib/hermes/holons/ResourceHolon.kt
+++ src/main/kotlin/pt/nfriacowboy/presib/hermes/holons/ResourceHolon.kt
@@ -5,9 +5,36 @@ import pt.nfriacowboy.presib.hermes.utils.HolonProperties
 
 class ResourceHolon(val holonId: String) : PresibHolon(holonId) {
 
-    val services: List<String> = holonConfig.config[HolonProperties.HOLON_SERVICES].split(",")
-    val topicNet: String = systemConfig.config[DefaultProperties.HOLON_MQTT_TOPIC_NET]
-    val topicDevice: String = systemConfig.config[DefaultProperties.HOLON_MQTT_TOPIC_DEVICE]
+    private val reserverServices:MutableMap<String, MutableList<String>> = mutableMapOf()
+
+    fun services(): List<String>{
+      return holonConfig.config[HolonProperties.HOLON_SERVICES].split(",");
+    }
+    fun topicNet(): String {
+        return "${systemConfig.config[DefaultProperties.HOLON_MQTT_TOPIC_NET]}$holonId"
+    }
+
+    fun topicDevice(): String {
+        return "${systemConfig.config[DefaultProperties.HOLON_MQTT_TOPIC_DEVICE]}$holonId"
+    }
+
+    fun currentId(): String {
+        return holonId;
+    }
+
+    fun reserveService(service:String, requesterHolonId:String) {
+        if(reserverServices[service] == null){
+            reserverServices[service] = mutableListOf(requesterHolonId)
+        }else {
+            reserverServices[service]?.add( requesterHolonId)
+        }
+    }
+
+    fun freeService(service: String, requesterHolonId: String){
+        reserverServices[service]?.remove(requesterHolonId)
+    }
 
 
 }
+
+//}IEC 61499
